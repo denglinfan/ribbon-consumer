e74a63ef07dfe23fefc785df71a68f124bfc99e5
charles <1061883381@qq.com>
2018-05-18 23:18:47 +0800

    add eureka consumer test 客户端负载

---
+++ src/main/java/com/charles/ribbonconsumer/RibbonConsumerApplication.java
@@ -0,0 +1,28 @@
+package com.charles.ribbonconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RibbonConsumerApplication {
+
+	/**
+	 * 创建restTemplate的Spring bean实例
+	 * 并通过@LoadBalanced注解开启客户端负载均衡
+	 * @return
+	 */
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonConsumerApplication.class, args);
+	}
+}
