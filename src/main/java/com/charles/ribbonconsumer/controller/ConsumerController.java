6f2abfa10871582ab98f6a132b179e1fdf7abee6
charles <1061883381@qq.com>
2018-05-27 15:20:11 +0800

    practise git command-line

--- src/main/java/com/charles/ribbonconsumer/controller/ConsumerController.java
+++ src/main/java/com/charles/ribbonconsumer/controller/ConsumerController.java
@@ -20,6 +20,10 @@ public class ConsumerController {
 
     @RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
     public String helloConsumer(){
+        /**
+         * getForEntity()方法第二个参数表示body的对象类型
+         * 该方法有三种不同的方法重载实现
+         */
         return restTemplate.getForEntity("http://HELLO-CHARLES/hello",String.class).getBody();
     }
 }
---
+++ src/main/java/com/charles/ribbonconsumer/controller/testGitConmand.java
@@ -0,0 +1,10 @@
+package com.charles.ribbonconsumer.controller;
+
+/**
+ * Created with IDEA
+ * Description: test git command line
+ * User: Charles
+ * Date: 2018-05-27 15:19
+ */
+public class testGitConmand {
+}
