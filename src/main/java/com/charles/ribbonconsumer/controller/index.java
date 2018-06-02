3cd4073c8d3a848e1680f2d6c675d77737ad439e
charles <1061883381@qq.com>
2018-06-02 09:37:48 +0800

    practise git command

---
+++ src/main/java/com/charles/ribbonconsumer/controller/index.java
@@ -0,0 +1,10 @@
+package com.charles.ribbonconsumer.controller;
+
+/**
+ * Created with IDEA
+ * Description: practise git command
+ * User: Charles
+ * Date: 2018-06-02 09:34
+ */
+public class index {
+}
